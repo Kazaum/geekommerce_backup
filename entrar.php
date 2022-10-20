38eb3cb1eda1ee963bc9e6f36eae2cfb3222b4a4
Kauan Pinheiro <kauan.pinheiro.1502@gmail.com>
2022-10-20 16:50:41 -0300

    first commit

---
+++ entrar.php
@@ -0,0 +1,3 @@
+<?php
+    include("assets/html/entrar.html");
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+include("assets/html/head.html");
+include("assets/html/header.html");
+include("assets/html/home.html");
+include("assets/html/incluirJS.html");
+
+?>
